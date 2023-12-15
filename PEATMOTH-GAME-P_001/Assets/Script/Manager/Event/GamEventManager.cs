ef9728908455a6ebf67e5bd8b35a22d24a04f7a7
aidendenden233 <2996876880@qq.com>
2023-12-16 00:38:00 +0800

    update 添加了Action的用法

--- PEATMOTH-GAME-P_001/Assets/Script/Manager/Event/GamEventManager.cs
+++ PEATMOTH-GAME-P_001/Assets/Script/Manager/Event/GamEventManager.cs
@@ -92,3 +92,103 @@ public class GameEventManager : MonoBehaviour
 // GameEventManager.Instance.Triggered("to touch",transform);
 
 #endregion
+
+
+#region Aciton的用法
+
+// class ActionFunction
+// {
+//     // 在目标脚本中定义带参数的Action
+//     public Action<int, string> MyAction;
+//     
+//     // 定义一个不带参数和返回值的Action
+//     Action _myAction = () => {
+//         // 这里是Action的具体实现
+//         Debug.Log("Action被调用");
+//     };
+//     
+//     // 定义一个带两个整数参数的Action
+//     Action<int, float> myAction = (intParam, floatParam) => {
+//         // 这里是Action的具体实现
+//         Debug.Log("Action被调用，参数为：" + intParam + ", " + floatParam);
+//     };
+//     
+//     public void A()
+//     {
+//         
+//          
+//                
+//            // 调用Action
+//            _myAction();
+//                
+//                
+//            // 定义一个接受Action作为参数的方法
+//            void DoSomething(Action action) {
+//                // 调用传递进来的Action
+//                action();
+//            }
+//        
+//            // 调用方法并传递Action
+//            DoSomething(_myAction);
+//        
+//            // 定义一个接受Action作为回调的方法
+//            void PerformTaskWithCallback(Action callback) {
+//                // 执行任务
+//                Debug.Log("执行任务中...");
+//        
+//                // 任务完成后调用回调
+//                if (callback != null) {
+//                    callback();
+//                }
+//            }
+//        
+//            // 定义一个回调方法
+//            void MyCallback() {
+//                Debug.Log("任务完成，回调被调用");
+//            }
+//        
+//            // 调用方法，并传递回调函数
+//            PerformTaskWithCallback(MyCallback);
+//            
+//           
+//            
+//            // 调用Action并传递参数
+//            myAction(10, 3.14f);
+//            
+//            // 定义一个接受带参数Action的方法
+//            void DoSomethingWithParams(Action<int, string> action) {
+//                // 调用传递进来的Action，传递参数
+//                action(42, "Hello");
+//            }
+//        
+//            // 调用方法并传递带参数的Action
+//            DoSomethingWithParams((intParam, stringParam) => {
+//                Debug.Log("参数为：" + intParam + ", " + stringParam);
+//            }); 
+//            
+//            // 在某个适当的时机调用Action，并传递参数
+//            if (MyAction != null) {
+//                int intValue = 10;
+//                string stringValue = "Hello";
+//                MyAction(intValue, stringValue);
+//            }
+//     }
+// }
+//
+// class MyClass
+// {
+//     // 在其他脚本中获取目标脚本的引用
+//     public ActionFunction targetScript;
+//
+//
+//     // 订阅或调用目标脚本中的Action
+//     void SubscribeToAction() {
+//         targetScript.MyAction += MyActionCallback;
+//     }
+//
+//     void MyActionCallback(int intValue, string stringValue) {
+//         Debug.Log("接收到参数：" + intValue + ", " + stringValue);
+//     }
+// }
+
+#endregion
